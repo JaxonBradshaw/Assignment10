b8ce2411bf86fdcf11ece74de611784a49e5ca92
JaxonBradshaw <71052977+JaxonBradshaw@users.noreply.github.com>
2021-04-05 16:45:25 -0600

    Assignment10

---
+++ Components/BowlerTeamViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Assignment10.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment10.Components
+{
+    public class BowlerTeamViewComponent : ViewComponent
+    {
+        private BowlingLeagueContext context;
+
+        //pulling in data
+        public BowlerTeamViewComponent(BowlingLeagueContext ctx)
+        {
+            context = ctx;
+        }
+        public IViewComponentResult Invoke ()
+        {
+            //ViewBag.SelectedTeam == RouteData?.Values["team"];
+            //setting up what will be returned
+            return View(context.Teams
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Assignment10.Models;
+using Assignment10.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment10.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private BowlingLeagueContext context { get; set; }
+        public HomeController(ILogger<HomeController> logger, BowlingLeagueContext ctx)
+        {
+            _logger = logger;
+            context = ctx;
+        }
+
+        //bringing in parameters
+        public IActionResult Index(long? teamid, string team, int pageNum = 0)
+        {
+            int pageSize = 5;
+
+            //setting viewbag
+            ViewBag.SelectedTeam = team;
+
+            return View(new IndexViewModel
+            {
+                Bowlers = (context.Bowlers
+                    .Where(b => b.TeamId == teamid || teamid == null)
+                    .OrderBy(b => b.BowlerFirstName)
+                    .Skip((pageNum - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToList()),
+
+                PageNumberingInfo = new PageNumberingInfo
+                {
+                    NumItemsPerPage = pageSize,
+                    CurrentPage = pageNum,
+                    //if teamid is null just do the count, otherwise count from the team that is selected
+                    TotalNumItems = teamid == null ? context.Bowlers.Count() :
+                        context.Bowlers.Where(x => x.TeamId == teamid).Count()
+                },
+
+                Team = team
+            });
+
+            return View(context.Bowlers.ToList());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
